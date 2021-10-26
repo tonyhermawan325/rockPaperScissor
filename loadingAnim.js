3c9bfc06d0f899e2a443ed7b2500cedd77255c10
Ludwigvan <tonyhermawan325@gmail.com>
2021-10-26 14:18:18 +0700

    adding loading animation, html structure for UI

--- loadingAnim.js
+++ loadingAnim.js
@@ -1,11 +1,2 @@
-const loaderTitle = document.querySelectorAll(".loaderTitle");
-const y = 0;
-for(let i = 0; i > 4; i++){
-    setTimeout(() => {
-        loaderTitle[1].style.opacity = "0"
-    }, 500);
-    clearTimeout();
-    loaderTitle[1].style.opacity = "1";
-}
 
 
