a45ee43e8987086063649a0a7abfa14638b77d1f
Ludwigvan <tonyhermawan325@gmail.com>
2021-10-27 11:09:51 +0700

    adding core rps gameplay script and logic, on the way to make gameplay verifying system

--- loadingAnim.js
+++ loadingAnim.js
@@ -1,11 +1,10 @@
-function compareMove(userMove, compMove){
-
-    if((this.compMove == "Paper" && this.userMove == "Rock") || (this.compMove == "Rock" && this.userMove == "Scissor") || (this.compMove == "Scissor" && this.userMove == "Paper")){
+function compareMove(plyMove, cmpMove){
+    if((cmpMove == 'Paper' && plyMove == 'Rock') || (cmpMove == 'Rock' && plyMove == 'Scissor') || (cmpMove == 'Scissor' && plyMove == 'Paper')){
         return "Computer Win";
     }
 
-    else if((this.compMove == "Paper" && this.userMove == "Scissor") || (this.compMove == "Rock" && this.userMove == "Paper" ) || (this.compMove == "Sissor" && this.userMove == "Rock")){
-        return "userWind"
+    else if((cmpMove == 'Paper' && plyMove == 'Scissor') || (cmpMove == 'Rock' && plyMove == 'Paper' ) || (cmpMove == 'Scissor' && plyMove == 'Rock')){
+        return "Player Win";
     }
 
     else{
@@ -13,10 +12,50 @@ function compareMove(userMove, compMove){
     }
 }
 
-const moveset = ["Rock","Paper","Scissor"];
-let userMove = document.querySelector('#userInput').value;
-    compMove = moveset[Math.floor(Math.random() * moveset.length)];
+function verify_userMove(userMove, log){
+    if ((this.userMove == "Rock") || (this.userMove == "Paper") || (this.userMove == "Scissor")){
+        return true;
+    }
+    else if (this.userMove == ""){
+        log.textContent = "Please, enter your move";
+        return false;
+    }
+    else{
+        log.textContent = "Invalid Move!";
+        return false;
+    }
+}
+    
+function playRound(){
+    let userMove_verified = new Boolean(false);
+    const moveset = ["Rock","Paper","Scissor"];
+    let userMove = document.querySelector('#userInput').value;
+    let compMove = moveset[Math.floor(Math.random() * moveset.length)];
+    const log = document.querySelector('#log');
 
-const log = document.querySelector('#log');
-log.textContent = compareMove(userMove, compMove);
-console.log(compareMove(userMove, compMove));
+    userMove_verified = verify_userMove(userMove, log);
+
+    if(userMove_verified == true){
+        log.textContent = compareMove(userMove, compMove);
+        document.querySelector('#compInput > p').textContent = compMove;
+        console.log(document.querySelector('#compInput > p'));
+    }
+}
+
+const button = document.querySelector('input[type="button"]');
+button.addEventListener('click', updateButton);
+
+function reset(){
+
+
+}
+
+
+function updateButton(e){
+    if(e.target.getAttribute("value") == "Play!" || e.target.getAttribute("value") == "Play Again?" ){
+        playRound();
+        e.target.value = "Play Again?";
+    }
+
+}
+
